f97d831cf6dc37887047021f331ac27b22eb3ce0
Raiymbek Kurmetkhan <RKurmetkhan@dar.lcl>
2021-02-24 17:51:11 +0600

    chore(): added downloading excel

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,36 +13,188 @@ document.querySelector('#footer').innerHTML = renderedFooter;
 
 const buttonCurrentText = $getDataBtn.textContent;
 
-function checkAll1() {
+const excelFieldsData = [
+    {
+        name: 'Brand',
+        value: 'Brand',
+        rus: '',
+        checked: true,
+        disabled: true,
+    },
+    {
+        name: 'Article',
+        value: 'Article',
+        rus: '',
+        checked: true,
+        disabled: true,
+    },
+    {
+        name: 'Name',
+        value: 'Name',
+        rus: '',
+        checked: true,
+        disabled: true,
+    },
+    {
+        name: 'CleanArticle',
+        value: 'CleanArticle',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'Presence',
+        value: 'Presence',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'CurrencyCode',
+        value: 'CurrencyCode',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'ExpectedShipmentDays',
+        value: 'ExpectedShipmentDays',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'GuaranteedShipmentDays',
+        value: 'GuaranteedShipmentDays',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'CountInPack',
+        value: 'CountInPack',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'WarehouseId',
+        value: 'WarehouseId',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'Warehouse',
+        value: 'Warehouse',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'Unit',
+        value: 'Unit',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'CategoryId',
+        value: 'CategoryId',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'ItemId',
+        value: 'ItemId',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'Discount',
+        value: 'Discount',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'TotalDiscount',
+        value: 'TotalDiscount',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'PriceWithoutDiscount',
+        value: 'PriceWithoutDiscount',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'IsnotProduced',
+        value: 'IsnotProduced',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'AvailableCount',
+        value: 'AvailableCount',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'SupplierCode',
+        value: 'SupplierCode',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'ExpectedDelivery',
+        value: 'ExpectedDelivery',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'GuaranteedDelivery',
+        value: 'GuaranteedDelivery',
+        rus: '',
+        checked: false,
+    },
+    {
+        name: 'DeliveryProbability',
+        value: 'DeliveryProbability',
+        rus: '',
+        checked: false,
+    },
+];
 
-    var inputs = document.querySelectorAll('.check1');
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].checked = true;
-    }
+const filterFieldsData = [
+    {
+        name: 'Товары на складах Phaeton',
+        value: 'phaetonStocks',
+        rus: '',
+        checked: true,
+        disabled: true,
+    },
+    {
+        name: 'Товары локальных поставщиков',
+        value: 'localSuppliers',
+        rus: '',
+        checked: true,
+        disabled: false,
+    },
+    {
+        name: 'Товары удаленных поставщиков',
+        value: 'remoteSuppliers',
+        rus: '',
+        checked: true,
+        disabled: false,
+    },
+];
 
-    this.onclick = uncheckAll1;
-}
+const excelCheckboxTemplate = document.querySelector('#checkbox-template').innerHTML;
 
-function uncheckAll1() {
-    var inputs = document.querySelectorAll('.check1');
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].checked = false;
-    }
-
-    this.onclick = checkAll1; //function reference to original function
-}
-
-var el = document.getElementById("checkall1"); //let for ES6 aficionados
-el.onclick = checkAll1; //again, function reference, no ()
-
-socket.on('message', (message) => {
-    console.log("message", message)
-    // const html = Mustache.render(messageTemplate, {
-    //     username: message.username,
-    //     message: message.text,
-    //     createdAt: moment(message.createdAt).format('h:mm a')
-    // })
+const renderExcelCheckBoxes = Mustache.render(excelCheckboxTemplate, {
+    checkboxList: excelFieldsData,
+    className: 'excel-checkbox'
 })
+
+const filterExcelCheckBoxes = Mustache.render(excelCheckboxTemplate, {
+    checkboxList: filterFieldsData,
+    className: 'filter-checkbox'
+})
+
+document.querySelector('#excel-checkboxes-wrapper').innerHTML = renderExcelCheckBoxes;
+document.querySelector('#filter-checkboxes-wrapper').innerHTML = filterExcelCheckBoxes;
 
 
 socket.on('excelFormationProcess', (messages) => {
@@ -53,10 +205,69 @@ socket.on('excelFormationProcess', (messages) => {
     document.querySelector('#logs_wrapper').innerHTML = html
 })
 
+// const stream = ss.createStream();
+// let binaryString = "";
+//
+// ss(socket).emit('excelFile', stream);
+//
+// stream.on('data', function(data) {
+//     for(var i=0;i<data.length;i++) {
+//         binaryString+=String.fromCharCode(data[i]);
+//     }
+// });
+//
+// stream.on('end', function() {
+//     console.log('end');
+//
+//     console.log(window.btoa(binaryString))
+// });
+
+ss(socket).on('excelFile', function(stream,data) {
+    console.log('received', data, stream);
+
+    let binaryString = "";
+
+    stream.on('data', function (data) {
+        console.log('data')
+
+        for (let i = 0; i < data.length; i++) {
+            binaryString += String.fromCharCode(data[i]);
+        }
+
+    });
+
+    stream.on('end', function (data) {
+        console.log('end')
+        const url = "data:application/vnd.ms-excel;base64," + window.btoa(binaryString);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = `PhaetonParsedData.xlsx`;
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    });
+});
+
 $getDataBtn.addEventListener('click', (e) => {
     $getDataBtn.textContent = 'Loading...';
     $getDataBtn.setAttribute('disabled', 'true');
-    socket.emit('getStockData', {}, (error) => {
+
+    const filterChecked = {};
+    const excelChecked = {};
+    const selectedCity = document.querySelector('.selectpicker').value;
+    document.querySelectorAll('.filter-checkbox').forEach(checkbox => {
+        if (checkbox.checked === true) {
+            filterChecked[checkbox.value] = checkbox.checked;
+        }
+    })
+
+    document.querySelectorAll('.excel-checkbox').forEach(checkbox => {
+        if (checkbox.checked === true) {
+            excelChecked[checkbox.value] = checkbox.checked;
+        }
+    })
+
+    socket.emit('getStockData', { excelChecked, filterChecked, selectedCity }, (error) => {
         $getDataBtn.textContent = buttonCurrentText;
         $getDataBtn.removeAttribute('disabled');
         console.log('Message delivered!')
--- src/index.js
+++ src/index.js
@@ -11,45 +11,69 @@ const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
+const bf = require('bruteforce');
+const fs = require('fs')
 
 
 app.use(express.static(publicDirectoryPath))
 
+
+const credKeys = {
+    almaty: {
+        guid: '19142b51-6518-11eb-8112-f8f21e09280d',
+        apiKey: 'EU8USEQ1VXm0LsLguS6Z',
+    },
+};
+
 io.on('connection', (socket) => {
     socket.on('getStockData', (message, callback) => {
+
         let workbook = new Excel.Workbook();
         let worksheet = workbook.addWorksheet('Stock data');
-        worksheet.columns = [
-            {header: 'First Name', key: 'firstName'},
-            {header: 'Last Name', key: 'lastName'},
-            {header: 'Purchase Price', key: 'purchasePrice'},
-            {header: 'Payments Made', key: 'paymentsMade'},
-            {header: 'Amount Remaining', key: 'amountRemaining'},
-            {header: '% Remaining', key: 'percentRemaining'}
-        ];
+        const worksheetColumns = [];
+
+        Object.keys(message.excelChecked).forEach(key => {
+            worksheetColumns.push({
+                header: key,
+                key: key,
+            })
+        });
+
+        worksheet.columns = worksheetColumns;
         worksheet.columns.forEach(column => {
             column.width = 20
         });
 
+        // const bf = require('bruteforce');
+        // const bruteForceData = bf({
+        //     len: 2,
+        //     chars: ['A', 'B', 1, 2, 3, 4, 5],
+        //     step: console.log
+        // });
+
+
+
+
         let successCounter = 0;
         let errorCounter = 0;
+
 
         for(let i = 0; i < 1000; i++) {
             // here request to get files
             successCounter ++;
             worksheet.addRow({
-                firstName: 'Harry',
-                lastName: 'Peake',
-                purchasePrice: 1000,
-                paymentsMade: 1000
+                Brand: 'Harry',
+                Article: 'Peake',
+                Name: 'some name',
             })
+
             io.emit('excelFormationProcess', { errorRequest: 0, successRequest: successCounter })
         }
 
-        // const stream = ss.createStream();
-        // ss(socket).emit('message', stream, {name: filename});
+        const stream = ss.createStream();
+        workbook.xlsx.write(stream);
+        ss(socket).emit('excelFile', stream, {});
 
-        // io.emit('message', workbook.xlsx.writeBuffer())
         callback()
     })
 
